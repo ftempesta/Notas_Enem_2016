31733e4a6872795a1db8f64d1cbabc2b31633c6b
MatheusGitMhub <matheus.pinto@ug.uchile.cl>
2020-06-07 15:58:55 -0400

    Regression, XGboost, SVR

--- codenation_library.py
+++ codenation_library.py
@@ -1,17 +1,34 @@
+# Preprocessing
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
+
+# Stats models
 import statsmodels.api as sm
 import pandas as pd
 import numpy as np
+
+# Machine learning
 import xgboost
 from xgboost import plot_importance
 from matplotlib import pyplot
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
+from sklearn.svm import SVR
+
+# Deep learning
+import keras
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, BatchNormalization
+from sklearn.metrics import mean_squared_error,mean_absolute_error
+import keras.backend as K
+
+# Visualization
 import matplotlib.pyplot as plt
 
 
-def LaberEncoder(dataset, categorical_columns):
+
+def labeL_encoder(dataset, categorical_columns):
     """
     Function to convert categorical columns to numerical
     applying label encoder
@@ -49,83 +66,143 @@ def OneHotEncoder(dataset, categorical_columns):
         
     categorical_columns : List
         Columns to convert to numerical 
-
     Returns
     -------
     Dataset with only numeric values applying one hot encoder
-
     """
     
     enc = OneHotEncoder(handle_unknown='ignore')
     enc_dataset =\
         pd.DataFrame(enc.fit_transform(dataset).toarray())
     dataset = dataset.join(enc_dataset)
+    dataset =\
+        dataset.drop(columns=categorical_columns).reset_index(drop=True)
         
     return dataset
 
 
-def LinearRegression(X,y):
+
+
+def mae_error_models(y_pred, y_test, min_y, max_y):
+    """
+    Function to get the Mean Absolute error of wholes models    
+
+    Parameters
+    ----------
+    y_pred : TYPE
+        DESCRIPTION.
+    y_test : TYPE
+        DESCRIPTION.
+    min_y : TYPE
+        DESCRIPTION.
+    max_y : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    mae_error : TYPE
+        DESCRIPTION.
+
+    """
+    y_pred_scaled = y_pred*(max_y-min_y) + min_y
+    y_test = y_test*(max_y-min_y)+min_y
+    
+    mae_error = abs((y_pred_scaled - y_test).mean())
+    
+    return mae_error
+
+def std_mae_error(y_pred, y_test, min_y, max_y):
+    """
+    Function to analyse standar distribution of mae error
+
+    Parameters
+    ----------
+    y_pred : TYPE
+        DESCRIPTION.
+    y_test : TYPE
+        DESCRIPTION.
+    min_y : TYPE
+        DESCRIPTION.
+    max_y : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    std_mae : TYPE
+        DESCRIPTION.
+
+    """
+    
+    y_pred_scaled = y_pred*(max_y-min_y) + min_y
+    y_test = y_test*(max_y-min_y)+min_y
+    
+    std_mae = (y_pred_scaled - y_test).std()
+    
+    return std_mae
+
+
+def linear_regression_processing(X_train, X_test, y_train, y_test):
     """
     Create a linear regression to this data to establish a base line of
     error on the next models
 
     Parameters
     ----------
-    X : Dataframe
-        Features.
-    y : Series
-        Targets.
+    X_train : Dataframe
+        Train features
+    X_train : Dataframe
+        Test feature     
+    y_train : Series
+        Train target
+    y_test : Series
+        Test target
 
     Returns
     -------
     return the error of the model
 
+    """    
+    X_train = sm.add_constant(X_train)
+    X_test = sm.add_constant(X_test)
+    
+    model = sm.OLS(y_train, X_train).fit()    
+    predictions = model.predict(X_test) 
+
+    return predictions
+
+
+
+def xgboost_processing(X_train, X_test, y_train,
+                       y_test, early_stopping_rounds,
+                       columns_dataset):
     """
     
-    X = sm.add_constant(X)
-    
-    model = sm.OLS(y, X).fit()
-    predictions = model.predict(X) 
-    
-    error =  model.rsquared
-
-    return error
-
-
-
-def xgboost_processing(X,y, early_stopping_rounds):
-    
-    """
-    Analyse feature importance with xgboost
 
     Parameters
     ----------
-    X : Dataframe
-        Features.
-    y : Series
-        Targets.
-    early_stopping_rounds: int
-        number of round in the boosting algorithm 
-        
-        
+    X_train : TYPE
+        DESCRIPTION.
+    X_test : TYPE
+        DESCRIPTION.
+    y_train : TYPE
+        DESCRIPTION.
+    y_test : TYPE
+        DESCRIPTION.
+    early_stopping_rounds : TYPE
+        DESCRIPTION.
+    columns_dataset : TYPE
+        DESCRIPTION.
 
     Returns
     -------
-    return the error of the model
-    
-    """
+    y_pred : TYPE
+        DESCRIPTION.
+    data : TYPE
+        DESCRIPTION.
 
-    # Normalizar carácterirsticas y etiquetas
-    sc_X = MinMaxScaler(feature_range = (0,1))
-    X = sc_X.fit_transform(X)
+    """
     
-    sc_y = MinMaxScaler(feature_range = (0,1))
-    y = y.to_numpy()
-    y = y.reshape(-1,1)
-    y = sc_y.fit_transform(y)
-    
-    # Dividir los conjuntos de datos
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    columns_X = columns_dataset
     
     # Reordenar en la forma (algo,)
     y_train = np.ravel(y_train)
@@ -144,40 +221,111 @@ def xgboost_processing(X,y, early_stopping_rounds):
     # Generar predicciones
     y_pred = regressor.predict(X_test)
     
-    min_y = sc_y.data_min_
-    min_y = min_y[0]
-    max_y = sc_y.data_max_
-    max_y = max_y[0]
+    feature_importance =\
+        regressor.get_booster().get_score(importance_type="weight")
     
-    # Re escalar los datos
-    y_pred_scaled = y_pred*(max_y-min_y) + min_y
-    y_test = y_test*(max_y-min_y)+min_y
-    
-    # Obtener el MAE Acumulado
-    mae_acum = abs(y_pred-y_test)
-    
-    # Promedio y desviación estandar de los mae's
-    total_mae = mae_acum.mean()
-    std = mae_acum.std()
-    
-    plt.figure(figsize=(40,20))
-    plt.scatter(y_test, y_pred_scaled, color = 'blue')
-    plt.scatter(y_test, y_test, color = 'red')
-    plt.title('Modelo "XGBRegressor"')
-    plt.xlabel('Cantidades reales')
-    plt.ylabel('Predicción XGBRegressor de cantidades')
-    plt.show()
-    
+    keys = list(feature_importance.keys())
+    values = list(feature_importance.values())
 
+    data = pd.DataFrame(data=values,
+                        index=keys,
+                        columns=["score"]).sort_values(by = "score",
+                                                       ascending=False)
+
+    list_feature_importance = []  
+
+    # Get the feature importance on a dataframe by column name
+    for col,score in zip(columns_X,regressor.feature_importances_):
+        list_feature_importance.append([col, score])
+        
+    data =pd.DataFrame(list_feature_importance,
+                     columns=["feature", "importance_score"])
+    data = data.sort_values(by="importance_score",
+                            ascending=False).reset_index(drop=True)
+    
+    
+    # Plot importance
     plt.figure(figsize=(40,20))
     plot_importance(regressor)
     pyplot.show()
     plt.show()
 
-    return total_mae, std, mae_acum
+    return  y_pred, data
+
+
+def support_vector_machines_processing(X_train, X_test, y_train, y_test):
+    """
+    
+
+    Parameters
+    ----------
+    X_train : TYPE
+        DESCRIPTION.
+    X_test : TYPE
+        DESCRIPTION.
+    y_train : TYPE
+        DESCRIPTION.
+    y_test : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    y_pred : TYPE
+        DESCRIPTION.
+
+    """
+
+    y_train = np.ravel(y_train)
+    y_test = np.ravel(y_test)
+    
+    # Support vector machines regressor
+    regressor = SVR(kernel='rbf')
+    
+    # Train model
+    regressor.fit(X_train,y_train)
+    
+    # Generate predictions
+    y_pred = regressor.predict(X_test)
+
+    return y_pred
 
 
 
+def plot_results(y_test, y_pred, model_name, min_y, max_y):
+    """
+    
 
+    Parameters
+    ----------
+    y_test : TYPE
+        DESCRIPTION.
+    y_pred : TYPE
+        DESCRIPTION.
+    model_name : TYPE
+        DESCRIPTION.
+    min_y : TYPE
+        DESCRIPTION.
+    max_y : TYPE
+        DESCRIPTION.
 
+    Returns
+    -------
+    TYPE
+        DESCRIPTION.
 
+    """
+    
+    y_pred_scaled = y_pred*(max_y-min_y) + min_y
+    y_test = y_test*(max_y-min_y)+min_y
+
+    # Plot of real values vs predicted
+    plt.figure(figsize=(20,10))
+    plt.scatter(y_test, y_pred_scaled, color = 'blue')
+    plt.scatter(y_test, y_test, color = 'red')
+    plt.title('Model :'+ " "+model_name, fontsize=30)
+    plt.xlabel('Math test values', fontsize=30)
+    plt.ylabel('Predictions', fontsize=30)
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+    
+    return plt.show()
--- test_prediction.py
+++ test_prediction.py
@@ -16,19 +16,42 @@ from codenation_library import *
 # ============================================================
 
 # Load_data
-dataset = pd.read_csv("testfiles/train.csv", index_col=0)
+dataset = pd.read_csv('testfiles/train.csv', index_col=0)
 
 # Describe
 dims = dataset.shape
 describe = dataset.describe()
 
 # ============================================================
-# Eliminete NaN
+# Eliminete NaN's in NU_NOTA_MT and irrelevats columns
 # ============================================================
 
 # Drop rows with nan values in the NU_NOTA_MT column
-nulos = dataset.isnull()["NU_NOTA_MT"].value_counts()
-dataset = dataset.dropna(subset=["NU_NOTA_MT"]).reset_index(drop=True)
+nulos = dataset.isnull()['NU_NOTA_MT'].value_counts()
+dataset = dataset.dropna(subset=['NU_NOTA_MT']).reset_index(drop=True)
+
+
+# Delete irrelevant columns 
+columns_dataset = dataset.columns.to_list()
+
+columns_to_delete = ['NU_ANO', 'CO_MUNICIPIO_RESIDENCIA',
+                     'SG_UF_RESIDENCIA', 'CO_MUNICIPIO_NASCIMENTO',
+                     'SG_UF_NASCIMENTO', 'CO_ESCOLA', 'CO_MUNICIPIO_ESC', 
+                     'SG_UF_ESC', 'SG_UF_ENTIDADE_CERTIFICACAO',
+                     'CO_MUNICIPIO_PROVA', 'SG_UF_PROVA', 'CO_PROVA_CN',
+                     'CO_PROVA_CH', 'CO_PROVA_LC', 'CO_PROVA_MT',
+                     'TX_RESPOSTAS_CN', 'TX_RESPOSTAS_CH', 'TX_RESPOSTAS_LC',
+                     'TX_RESPOSTAS_MT', 'TX_GABARITO_CN', 'TX_GABARITO_CH',
+                     'TX_GABARITO_LC', 'TX_GABARITO_MT', 'TP_STATUS_REDACAO',
+                     'NU_NOTA_COMP1', 'NU_NOTA_COMP2', 'NU_NOTA_COMP3',
+                     'NU_NOTA_COMP4', 'NU_NOTA_COMP5',
+                     'NO_MUNICIPIO_NASCIMENTO']
+
+dataset = dataset.drop(columns=columns_to_delete)
+
+# ============================================================
+# Eliminete columns with less than 50 % of the values
+# ============================================================
 
 # Count nan values by columns
 null_columns =\
@@ -50,50 +73,212 @@ dataset_filtered = dataset[columns_correct]
 # Filtered dataset with the columns selected before 
 dataset = dataset_filtered.dropna()
 
+# Target: inscription number and score math testv
+target = dataset[['NU_INSCRICAO','NU_NOTA_MT']]
+dataset = dataset.drop(columns=['NU_INSCRICAO','NU_NOTA_MT'])
+
 # ============================================================
 # Transform categorical to numeric
 # ============================================================
 
-# Target: inscription number and score math testv
-target = dataset[["NU_INSCRICAO","NU_NOTA_MT"]]
-
-dataset = dataset.drop(columns=["NU_INSCRICAO","NU_NOTA_MT"])
-
-
-# Seleccionar columnas categoricas
+#label_encoder_columns = ['TP_SEXO', 'NO_MUNICIPIO_RESIDENCIA', 
+#'NO_MUNICIPIO_PROVA']
 categorical_columns =\
-    dataset.select_dtypes(include=['object']).columns.to_list()
+     dataset.select_dtypes(include=['object']).columns.to_list()
+dataset = labeL_encoder(dataset, categorical_columns)
 
-# Apply laber encoder to categorical columns
-dataset = LaberEncoder(dataset, categorical_columns)
+# # Select categorical columns
+# categorical_columns =\
+#     dataset.select_dtypes(include=['object']).columns.to_list()
+# # Convert categorical columns into numerical column
+# dataset = one_hot_encoder(dataset, categorical_columns)
 
 # ============================================================
-# Basic linear regression
+# Data preparation
 # ============================================================
 
-X = dataset
+X = dataset_important
+# Columns 
+columns_dataset = X.columns
+
 y = target["NU_NOTA_MT"]
 
-# Error of the linear regression
-error_r2 =  LinearRegression(X,y)
-print(error_r2)
+
+# Normalize features and split
+sc_X = MinMaxScaler(feature_range = (0,1))
+X = sc_X.fit_transform(X)
+
+sc_y = MinMaxScaler(feature_range = (0,1))
+y = y.to_numpy()
+y = y.reshape(-1,1)
+y = sc_y.fit_transform(y)
+
+# Min and max to rescale the data
+min_y = sc_y.data_min_
+min_y = min_y[0]
+max_y = sc_y.data_max_
+max_y = max_y[0]
+
+# Split datasets
+X_train, X_test, y_train, y_test =\
+    train_test_split(X, y, test_size=0.2, random_state=42)
 
 
 # ============================================================
-# Feature importance with XGBoost
+# Data preparation
+# ============================================================
+
+# Predictions of the linear regression
+y_pred_regression =  linear_regression_processing(X_train, X_test,
+                                                  y_train, y_test)
+# MAE of regression
+mae_regression =\
+    mae_error_models(y_pred_regression, y_test, min_y, max_y)
+# Standart deviation of error
+std_mae_regression =\
+    std_mae_error(y_pred_regression, y_test, min_y, max_y)
+
+model_name = "Regression"
+plot_results(y_test, y_pred_regression,
+             model_name, min_y, max_y)
+
+
+# ============================================================
+# XGBoost model
 # ============================================================
 
 # Number of steps on the boosting algorithm
 early_stopping_rounds = 200
 
-# Mean absolute error and std of mae error of xgboost algorithm
-mae_xgboost, std_xgboost, mae_acum =\
-    xgboost_processing(X,y, early_stopping_rounds)
+# Mean absolute error and std of mae error of xgboost algorithm for y:test
+y_pred_xgboost, feature_importance =\
+    xgboost_processing(X_train, X_test, y_train,
+                       y_test, early_stopping_rounds,
+                       columns_dataset)
+
+# MAE of xgboost algorithm
+mae_xgboost =\
+    mae_error_models(y_pred_xgboost, y_test, min_y, max_y)
+# Standart deviation of error
+std_mae_xgboost =\
+    std_mae_error(y_pred_xgboost, y_test, min_y, max_y)
+
+# Plot results XGBoost
+model_name = "XGboost"
+plot_results(y_test, y_pred_xgboost, model_name, min_y, max_y)
+
+
+# ============================================================
+# Support vector regressor
+# ============================================================
+
+y_pred_svr =\
+    support_vector_machines_processing(X_train, X_test, y_train, y_test)
+
+# MAE of SVR algorithm
+mae_svr =\
+    mae_error_models(y_pred_svr, y_test, min_y, max_y)
+    
+# Standard deviation of mae
+std_mae_svr =\
+    std_mae_error(y_pred_svr, y_test, min_y, max_y)
+
+# Plot results SVR
+model_name = "Support vector regressor"
+plot_results(y_test, y_pred_svr, model_name, min_y, max_y)
+
+
+# ============================================================
+# Neural networks
+# ============================================================
+
+def neural_net_processing():
+    
+    
 
 
 
 
 
+# Select the 30° most important columns
+
+feature_importance = feature_importance.iloc[:30]
+
+important_columns = (feature_importance["feature"]).to_list()
+
+# Calculate correlation just in features importante
+dataset_important = dataset[important_columns]
+
+correlation_feature_important =\
+    dataset_important.corr().unstack().\
+        sort_values().drop_duplicates().reset_index(drop=False)
+
+correlation_feature_important.columns = ["var1", "var2", "correlation"]
+
+correlation_feature_important =\
+    correlation_feature_important.sort_values(by=["correlation"],
+                                                      ascending=False).\
+    iloc[1:].reset_index(drop=True)
+
+
+
+
+
+
+
+
+
+
+# Dataframe of correlation between columns (complete dataset)
+correlation_columns =\
+    dataset.corr().unstack().sort_values().\
+        drop_duplicates().reset_index(drop=False)
+correlation_columns.columns = ["var1", "var2", "correlation"]
+correlation_columns = correlation_columns.sort_values(by=["correlation"],
+                                                      ascending=False).\
+    iloc[1:].reset_index(drop=True)
+
+
+# High correlation columns
+high_correlated_columns =\
+    correlation_columns[(correlation_columns['correlation'] > 0.25) |
+                        (correlation_columns['correlation'] < -0.25)]
+
+
+
+
+
+
+
+# correlation_importance_selection1 = pd.merge(feature_importance,
+#                                             high_correlated_columns,
+#                                             how='outer',
+#                                             left_on=['feature'],
+#                                             right_on =['var1']).dropna()
+
+# correlation_importance_selection2 = pd.merge(feature_importance,
+#                                             high_correlated_columns,
+#                                             how='outer',
+#                                             left_on=['feature'],
+#                                             right_on =['var2']).dropna()
+
+# correlation_importance_selection = pd.merge(correlation_importance_selection1,
+#                                             correlation_importance_selection2,
+#                                             how='outer',
+#                                             left_on=['feature'],
+#                                             right_on =['feature']).dropna()
+
+
+# for ii in range(correlation_importance_selection.shape[0]):
+    
+#     var2 = correlation_importance_selection["var2"].iloc[ii]
+    
+#     for jj in range(len(important_columns)):
+#         features =(important_columns[jj]
+
+#         if features==var:
+#             print("variable repetida")
+ 
 
 # # Aplicar PCA para encontrar componentes principales
 # pca = PCA(n_components=100)
